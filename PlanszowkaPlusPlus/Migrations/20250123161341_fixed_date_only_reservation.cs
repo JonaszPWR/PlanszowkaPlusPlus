15d029a6b98df25fbc93d7ee304824fd4ddf1db8
JonaszPWR <272577@student.pwr.edu.pl>
2025-01-23 17:17:59 +0100

    v0.5.A1 branch start

---
+++ PlanszowkaPlusPlus/Migrations/20250123161341_fixed_date_only_reservation.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanszowkaPlusPlus.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixed_date_only_reservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Employees",
+                newName: "PasswordHash");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReservationDate",
+                table: "Reservations",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_MemberId",
+                table: "Reservations",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_TableId",
+                table: "Reservations",
+                column: "TableId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_MemberId",
+                table: "Rentals",
+                column: "MemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Games_GameId",
+                table: "Rentals",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Members_MemberId",
+                table: "Rentals",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_GameTables_TableId",
+                table: "Reservations",
+                column: "TableId",
+                principalTable: "GameTables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Members_MemberId",
+                table: "Reservations",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Games_GameId",
+                table: "Rentals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Members_MemberId",
+                table: "Rentals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_GameTables_TableId",
+                table: "Reservations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Members_MemberId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_MemberId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_TableId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_MemberId",
+                table: "Rentals");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Employees",
+                newName: "Password");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReservationDate",
+                table: "Reservations",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
--- PlanszowkaPlusPlus/obj/Debug/net8.0/PlanszowkaPlusPlus.AssemblyInfo.cs
+++ PlanszowkaPlusPlus/obj/Debug/net8.0/PlanszowkaPlusPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanszowkaPlusPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d742988e64f14adffd3609e7a82c59c3b9c5af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784d8a876848e320937915196033a195e20f025d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanszowkaPlusPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanszowkaPlusPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
