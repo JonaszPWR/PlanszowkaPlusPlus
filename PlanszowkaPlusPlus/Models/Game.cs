35346e35e56816f9f62caf6ce6006aed05999b97
hetmanate1918 <272602@student.pwr.edu.pl>
2025-04-02 19:05:33 +0200

    v0.5.10 minor fix

--- PlanszowkaPlusPlus/Models/Game.cs
+++ PlanszowkaPlusPlus/Models/Game.cs
@@ -1,11 +1,15 @@
-namespace PlanszowkaPlusPlus.Models
-{
+namespace PlanszowkaPlusPlus.Models;
+using System.ComponentModel.DataAnnotations;
+
     public class Game
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be at least 0.")]
         public int TotalNumber { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be at least 0.")]
         public int AvailableNumber { get; set; }
     }
-}
